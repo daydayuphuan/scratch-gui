b09712290f61fb06d8e5eb8215a333a77756e8d0
Yanminge <guojia198769@163.com>
2020-08-11 21:49:36 +0800

    fix some language

--- src/components/firmware-update-tip/firmware-update-tip.jsx
+++ src/components/firmware-update-tip/firmware-update-tip.jsx
@@ -13,12 +13,12 @@ const messages = defineMessages({
         id: 'gui.matata.firmwareUpdate'
     },
     firmwareUpdatePreLable: {
-        defaultMessage: 'The version get from current hardware: ',
+        defaultMessage: 'The current device firmware version: ',
         description: '',
         id: 'gui.matata.firmwareUpdatePreLable'
     },
     firmwareUpdatePostLable: {
-        defaultMessage: 'The firmware version does not match the version supported by the extension, needs to be upgraded',
+        defaultMessage: 'The firmware version does not match the version supported by the extension. Please upgrade the firmware.',
         description: '',
         id: 'gui.matata.firmwareUpdatePostLable'
     },
@@ -26,16 +26,90 @@ const messages = defineMessages({
         defaultMessage: 'Go to upgrade',
         description: '',
         id: 'gui.matata.firmwareUpdateButtonText'
+    },
+    sensorModeMismatch: {
+        defaultMessage: 'The MatataCon is not in sensor mode, please switch the mode and try again.',
+        description: '',
+        id: 'gui.matata.sensorModeMismatch'
+    },
+    carNeedUpdateFirmware: {
+        defaultMessage: 'Please upgrade the MatataBot which is connected to this MatataCon.',
+        description: '',
+        id: 'gui.matata.carNeedUpdateFirmware'
     }
 });
 
-const FirmareUpdateTip = ({intl, ...props}) => {
-    // 弹窗中的文字内容
-    const content =
-        `${intl.formatMessage(messages.firmwareUpdatePreLable) +
+const getBodyContent = (intl, props) => {
+    let content = `${intl.formatMessage(messages.firmwareUpdatePreLable) +
             props.deviceVersion} (${props.deviceType}) \n\n 
             ${intl.formatMessage(messages.firmwareUpdatePostLable)}`;
 
+    if(props.deviceVersion === "notSensorMode") {
+        return (
+            <Box className={styles.updateTipContainer}>
+                <div
+                    className={styles.tipText}
+                    style={{whiteSpace: 'pre-line'}}
+                >
+                    {intl.formatMessage(
+                        messages.sensorModeMismatch
+                    )}
+                </div>
+            </Box>);
+    } else if (props.deviceVersion === "carNotSupport") {
+        return (
+            <Box className={styles.updateTipContainer}>
+                <div
+                    className={styles.tipText}
+                    style={{whiteSpace: 'pre-line'}}
+                >
+                    {intl.formatMessage(
+                        messages.carNeedUpdateFirmware
+                    )}
+                </div>
+                <p className={styles.linkWrapper}>
+                    <a
+                        href={domain.mata_device_update_link}
+                        className={styles.mainButton}
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        {intl.formatMessage(
+                            messages.firmwareUpdateButtonText
+                        )}
+                    </a>
+                </p>
+            </Box>);
+    } else {
+        return (    
+            <Box className={styles.updateTipContainer}>
+                <div
+                    className={styles.tipText}
+                    style={{whiteSpace: 'pre-line'}}
+                >
+                    {content}
+                </div>
+                <p className={styles.linkWrapper}>
+                    <a
+                        href={domain.mata_device_update_link}
+                        className={styles.mainButton}
+                        rel="noreferrer"
+                        target="_blank"
+                    >
+                        {intl.formatMessage(
+                            messages.firmwareUpdateButtonText
+                        )}
+                    </a>
+                </p>
+            </Box>);
+    }
+};
+
+const FirmareUpdateTip = ({intl, ...props}) => {
+    // 弹窗中的文字内容
+    
+    const bodyContent = getBodyContent(intl, props);
+    
     return (
         <Modal
             id="matata.firmware.update.modal"
@@ -44,27 +118,7 @@ const FirmareUpdateTip = ({intl, ...props}) => {
             onRequestClose={props.onCancel}
         >
             <Box className={styles.body}>
-                <Box className={styles.updateTipContainer}>
-                    <div
-                        className={styles.tipText}
-                        style={{whiteSpace: 'pre-line'}}
-                    >
-                        {content}
-                    </div>
-
-                    <p className={styles.linkWrapper}>
-                        <a
-                            href={domain.mata_device_update_link}
-                            className={styles.mainButton}
-                            rel="noreferrer"
-                            target="_blank"
-                        >
-                            {intl.formatMessage(
-                                messages.firmwareUpdateButtonText
-                            )}
-                        </a>
-                    </p>
-                </Box>
+                {bodyContent}
             </Box>
         </Modal>
     );
